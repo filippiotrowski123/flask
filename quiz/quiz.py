c71874739d27a7ab62eabd55c11ce4fcae1e3d56
filippiotrowski123 <43171827+filippiotrowski123@users.noreply.github.com>
2020-04-06 11:25:11 +0200

    flask1

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -6,11 +6,39 @@ from flask import render_template
 
 app = Flask(__name__)
 
+# konfiguracja aplikacji
+app.config.update(dict(
+    SECRET_KEY='bradzosekretnawartosc',
+))
+
+# lista pytań
+DANE = [{
+    'pytanie': 'Stolica Hiszpani, to:',  # pytanie
+    'odpowiedzi': ['Madryt', 'Warszawa', 'Barcelona'],  # możliwe odpowiedzi
+    'odpok': 'Madryt'},  # poprawna odpowiedź
+    {
+    'pytanie': 'Objętość sześcianu o boku 6 cm, wynosi:',
+    'odpowiedzi': ['36', '216', '18'],
+    'odpok': '216'},
+    {
+    'pytanie': 'Symbol pierwiastka Helu, to:',
+    'odpowiedzi': ['Fe', 'H', 'He'],
+    'odpok': 'He'},
+    {
+    'pytania': 'Pole kwadratu o boku 4cm to',
+	'odpoiwedzi': ['16cm', '4cm', '8cm'],
+	'obok': '16'},
+ 
+
+]
+
+	
+
 @app.route('/')
 def index():
-    #return 'Cześć, tu Python!'
-    return render_template('index.html')
+    # return 'Cześć, tu Python!'
+    return render_template('index.html', pytania=DANE)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
